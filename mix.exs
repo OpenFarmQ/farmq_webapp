193515191f05232644d03e1e88ed2a869271158a
Nittin Shankar <nittin.shankar@gmail.com>
2018-11-18 10:31:33 +0530

    Add active link for sidebar

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule FarmQ.MixProject do
       {:phoenix_live_reload, "~> 1.2-rc", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_active_link, "~> 0.1.1"}
     ]
   end
 
