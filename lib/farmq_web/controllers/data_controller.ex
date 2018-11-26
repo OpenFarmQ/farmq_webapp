42854292a2eef2cbbc1c16a7a471000238511810
Nittin Shankar <nittin.shankar@gmail.com>
2018-11-26 21:40:52 +0530

    index crop-cycles in browse page

--- lib/farmq_web/controllers/data_controller.ex
+++ lib/farmq_web/controllers/data_controller.ex
@@ -1,7 +1,9 @@
 defmodule FarmQWeb.DataController do
   use FarmQWeb, :controller
+  alias FarmQ.Core
 
   def index(conn, _params) do
-    render conn, "index.html"
+    crop_cycles = Core.list_crop_cycles
+    render(conn, "index.html", crop_cycles: crop_cycles)
   end
 end
--- lib/farmq_web/views/data_view.ex
+++ lib/farmq_web/views/data_view.ex
@@ -1,4 +1,27 @@
 defmodule FarmQWeb.DataView do
   use FarmQWeb, :view
-  
+
+  def plant_name(crop_cycle) do
+    if crop_cycle.sowing_data do
+      crop_cycle.sowing_data.plant.name
+    else
+      "Sowing data not yet created"
+    end
+  end
+
+  def seed_weight(crop_cycle) do
+    if crop_cycle.sowing_data do
+      crop_cycle.sowing_data.seed_weight
+    else
+      "Sowing data not yet created"
+    end
+  end
+
+  def sown_date(crop_cycle) do
+    if crop_cycle.sowing_data do
+      crop_cycle.sowing_data.sown_date
+    else
+      "Sowing data not yet created"
+    end
+  end
 end
